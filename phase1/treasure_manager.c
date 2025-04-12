6985359c9b859cc61b5301c333b869f38039d808
Giulia <204734051+giulia-efrem@users.noreply.github.com>
2025-04-12 18:14:52 +0300

    Added addTreasure function

--- phase1/treasure_manager.c
+++ phase1/treasure_manager.c
@@ -78,6 +78,74 @@ int addHunt(const char *huntName)
     return 0;
 }
 
+int addTreasure(const char *huntName) 
+{
+
+    char huntDir[256];
+    snprintf(huntDir, sizeof(huntDir), "hunts/%s", huntName);
+
+
+    struct stat st;
+    if (stat(huntDir, &st) != 0 || !S_ISDIR(st.st_mode))
+    {
+        printf("Hunt '%s' doesn't exist, create it\n", huntName);
+        return -1;
+    }
+
+
+    char treasurePath[256];
+    snprintf(treasurePath, sizeof(treasurePath), "%s/treasure.bin", huntDir);
+    FILE *f = fopen(treasurePath, "ab");
+    if (!f) 
+    {
+        perror("Error opening treasure.bin");
+        return -1;
+    }
+
+
+    Treasure t;
+    printf("Treasure ID: ");
+    scanf("%d", &t.treasureId);
+
+    printf("Username: ");
+    scanf("%s", t.username);
+
+    printf("Latitude: ");
+    scanf("%f", &t.latitude);
+
+    printf("Longitude: ");
+    scanf("%f", &t.longitude);
+
+    printf("Clue: ");
+
+    getchar(); 
+    scanf("%[^\n]", t.clue);
+
+    printf(" Value: ");
+    scanf("%d", &t.value);
+
+
+    fwrite(&t, sizeof(Treasure), 1, f);
+    fclose(f);
+
+    char generalLogPath[256];
+    snprintf(generalLogPath, sizeof(generalLogPath), "logs/general.log");
+
+    char huntLogPath[256];
+    snprintf(huntLogPath, sizeof(huntLogPath), "logs/%s.log", huntName);
+
+    char logEntry[256];
+    snprintf(logEntry, sizeof(logEntry),
+             "Treasure ID %d added to hunt '%s' by user '%s'.",
+             t.treasureId, huntName, t.username);
+
+    logMessage(generalLogPath, logEntry);
+    logMessage(huntLogPath, logEntry);
+
+    printf("Treasure added to hunt '%s'.\n", huntName);
+    return 0;
+}
+
 int main(int argc, char *argv[]) 
 {
     if (argc < 2) 
@@ -103,6 +171,17 @@ int main(int argc, char *argv[])
     }
     else if (strcmp(argv[1], "--add_treasure") == 0) 
     {
+        if (argc < 3) 
+        {
+            printf("Use: %s --add_treasure <huntName>\n", argv[0]);
+            return 1;
+        }
+
+        if (addTreasure(argv[2]) != 0) 
+        {
+            printf("Failed to add treasure to hunt '%s'.\n", argv[2]);
+            return 1;
+        }
     }
     else if (strcmp(argv[1], "--list") == 0) 
     {
