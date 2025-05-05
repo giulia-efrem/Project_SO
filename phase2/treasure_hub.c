856b46b14a8bb388d41e597e857ebb98efcbce52
Giulia <204734051+giulia-efrem@users.noreply.github.com>
2025-05-05 23:52:46 +0300

    Updated monitor_loop and start_monitor functions

--- phase2/treasure_hub.c
+++ phase2/treasure_hub.c
@@ -3,19 +3,66 @@
 #include <stdlib.h>
 #include <sys/types.h>  
 #include <unistd.h>     
+#include <dirent.h>
+
+pid_t monitor_pid = 0;
+
+int fds[2]; 
+
+int only_dirs(const struct dirent *e) {
+    return (e->d_type == DT_DIR
+        && strcmp(e->d_name, ".")
+        && strcmp(e->d_name, ".."));
+}
 
 void monitor_loop() 
 {
-    printf("[Monitor %d] ready for commands…\n", getpid());
-    
+    printf("[Monitor %d] is ready for commands…\n", getpid());
+
     while (1) 
     {
-        pause();
+        char task[64];
+
+        ssize_t n = read(fds[0], task, sizeof(task)-1);
+
+        if (n > 0) {
+
+            task[n] = '\0';
+
+            if (strcmp(task, "list_hunts")==0) {
+                const char *path = "../phase1/hunts";
+                DIR *d = opendir(path);
+                if (!d) {
+                    perror("opendir");
+                    exit(0);
+                }
+
+                struct dirent *entry;
+                while ((entry = readdir(d)) != NULL) {
+                    if (entry->d_name[0] == '.' &&
+                        (entry->d_name[1] == '\0' ||
+                        (entry->d_name[1] == '.' && entry->d_name[2] == '\0')))
+                        continue;
+
+                    printf("%s\n", entry->d_name);
+                }
+
+                closedir(d);
+            }
+
+        }
+
+
     }
 }
 
 void start_monitor() 
 {
+    if (pipe(fds) == -1) 
+    {
+        perror("pipe");
+        exit(1);
+    }
     
     if (monitor_pid) 
     {
@@ -33,15 +80,20 @@ void start_monitor()
 
     if (pid == 0) 
     {
+        close(fds[1]); 
         monitor_loop();
         exit(0);
     }
+
+    close(fds[0]); // we make parent write-only
     monitor_pid = pid;
     printf("Started monitor %d\n", monitor_pid);
 }
 
 void list_hunts()      
 { 
+    const char *task = "list_hunts";
+    write(fds[1], task, strlen(task));
     
 }
 
